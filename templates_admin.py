b6d961bc40e3fdd96052afb396761fa282565dd4
alhyss <55992003+alhyss@users.noreply.github.com>
2021-06-11 10:01:26 +0200

    Update templates_admin.py

--- templates_admin.py
+++ templates_admin.py
@@ -7,17 +7,15 @@ from rdflib import Graph
 from rdflib.util import guess_format
 from rdflib.serializer import Serializer
 
-from rdf_utils import buildNSDict
 
-
-def importVocabulary(directory: str, gTemplate: Graph, language: str ="fr") -> Graph:
+def importVocabulary( directory: str, schemes: list, languages: list = [ "fr", "en" ] ) -> Graph:
     """Parse RDF SKOS ontologies into a graph.
 
     - directory est le chemin du répertoire où sont stockés les fichiers contenant
     les ontologies. Ne seront considérés que les fichiers .rdf et .ttl.
-    - gTemplate est un modèle de graphe qui explicite les ontologies à utiliser pour
-    les métadonnées via des propriétés skos:inScheme.
-    - la langue pour laquelle le vocabulaire sera importé. Français par défaut.
+    - schemes est la liste des IRI des ensembles de concepts à récupérer.
+    - languages est la liste des langues pour laquelle le vocabulaire sera importé.
+    Français et anglais par défaut.
 
     Résultat : un graphe contenant une version allégée des ontologies, avec
     pour chaque terme un objet de type skos:Concept avec ses propriétés
@@ -28,9 +26,6 @@ def importVocabulary(directory: str, gTemplate: Graph, language: str ="fr") -> G
         skos:inScheme <http://publications.europa.eu/resource/authority/data-theme> ;
         skos:prefLabel "Environnement"@fr .
 
-    Ne sont importés que les objets dont la propriété skos:inScheme est référencée
-    dans le modèle gTemplate.
-
     En supposant que les ontologies à traiter sont dans un sous-répertoire
     "ontologies" et que le modèle de graphe est un fichier dataset_template.ttl :
     >>> with open('dataset_template.ttl', encoding='UTF-8') as src:
@@ -39,10 +34,10 @@ def importVocabulary(directory: str, gTemplate: Graph, language: str ="fr") -> G
     >>> print(g.serialize(format="turtle").decode("utf-8"))
     """
 
-    prefixDict = buildNSDict(gTemplate)
-
     graph = Graph()
-    graph.namespace_manager = gTemplate.namespace_manager
+    graph.namespace_manager.bind('skos', URIRef('http://www.w3.org/2004/02/skos/core#'), override=True, replace=True)
+    
+    dskos = { 'skos' : URIRef('http://www.w3.org/2004/02/skos/core#') }
 
     gOntology = Graph()
 
@@ -51,17 +46,7 @@ def importVocabulary(directory: str, gTemplate: Graph, language: str ="fr") -> G
             with open(f, encoding='UTF-8') as src:
                 gOntology.parse(data=src.read(), format=guess_format(f))
 
-    q1 = gTemplate.query(
-            """
-            SELECT
-                ?o
-            WHERE
-                {{ ?s {} ?o }}
-            """.format("skos:inScheme"),
-            initNs = prefixDict
-            )
-
-    for r1 in q1:
+    for s in schemes:
 
         q2 = gOntology.query(
             """
@@ -73,7 +58,7 @@ def importVocabulary(directory: str, gTemplate: Graph, language: str ="fr") -> G
                     skos:prefLabel ?l .
                   FILTER ( lang(?l) = "fr" ) }
             """,
-            initNs = prefixDict,
+            initNs = dskos,
             initBindings = { "o": r1.o }
             )
 
