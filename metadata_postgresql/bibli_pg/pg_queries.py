6793e7b3587dffc8e6445b707d7bf15a87841e2c
alhyss <55992003+alhyss@users.noreply.github.com>
2021-09-11 22:25:56 +0200

    Update pg_queries.py
    Ajout de requêtes pour la gestion des modèles de formulaires.

--- metadata_postgresql/bibli_pg/pg_queries.py
+++ metadata_postgresql/bibli_pg/pg_queries.py
@@ -16,6 +16,7 @@ Dépendances : psycopg2-binary (https://www.psycopg.org).
 
 from psycopg2 import sql
 
+
 def query_update_table_comment(schema_name, table_name):
     """Crée une requête de mise à jour du descriptif d'une table ou vue.
     
@@ -39,4 +40,66 @@ def query_update_table_comment(schema_name, table_name):
         ).format(
             sql.Identifier(schema_name, table_name)
         )
+
+
+def query_list_templates():
+    """Crée une requête d'import de la liste des modèles disponibles.
+    
+    ARGUMENTS
+    ---------
+    Néant.
+    
+    RESULTAT
+    --------
+    Une requête prête à l'emploi, à utiliser comme suit :
+    >>> cur.execute(query_list_templates())
+    """
+    return """
+        SELECT
+            tpl_id,
+            tpl_label,
+            schema_prefix,
+            schema_suffix,
+            table_prefix,
+            table_suffix,
+            conditions,
+            priority
+            FROM z_metadata.meta_template
+            ORDER BY tpl_id
+        """
+
+
+def query_get_categories():
+    """Crée une requête d'import des catégories à afficher dans un modèle donné.
+    
+    ARGUMENTS
+    ---------
+    Néant.
+    
+    RESULTAT
+    --------
+    Une requête prête à l'emploi, à utiliser comme suit :
+    >>> cur.execute(query_get_categories(), (tpl_label,))
+    
+    Avec :
+    - tpl_label (str) : nom du modèle à utiliser.
+    """
+    return """
+        SELECT 
+            tplcat_id,
+            path,
+            cat_label,
+            widget_type,
+            row_span,
+            help_text,
+            default_value,
+            placeholder_text,
+            input_mask,
+            multiple_values,
+            is_mandatory,
+            order_key,
+            read_only
+            FROM z_metadata.meta_template_categories_full
+            WHERE tpl_label = %s
+        """
 
