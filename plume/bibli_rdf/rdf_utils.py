c9478eafeaa2f58161d104ebde55f5296586f524
alhyss <55992003+alhyss@users.noreply.github.com>
2021-10-28 10:58:46 +0200

    Update rdf_utils.py
    Les formats d'import / export et extensions correspondantes sont maintenant stockées dans un unique dictionnaire rdflib_formats. Ajout de fonctions pour faciliter son exploitation + modification en conséquence des fonction metagraph_from_file(), export_metagraph() et available_format().

--- plume/bibli_rdf/rdf_utils.py
+++ plume/bibli_rdf/rdf_utils.py
@@ -27,6 +27,7 @@ import re, uuid
 from plume.bibli_rdf import __path__
 
 
+
 class WidgetsDict(dict):
     """Classe pour les dictionnaires de widgets.
     """
@@ -3270,33 +3271,25 @@ def export_metagraph(metagraph, shape, filepath, format=None):
     """
     pfile = Path(filepath)
 
-    if format and not format in (
-        "turtle", "json-ld", "xml", "n3", "nt", "pretty-xml", "trig"
-        ):
+    if format and not format in export_formats():
         raise ValueError("Format '{}' is not supported.".format(format))
 
     for n, u in shape.namespace_manager.namespaces():
             metagraph.namespace_manager.bind(n, u, override=True, replace=True)
     
-    # extensions possibles et formats correspondants :
-    d = {".ttl": "turtle", ".n3": "n3", ".json": "json-ld",
-         ".jsonld": "json-ld", ".xml": "pretty-xml", ".nt": "nt",
-         ".rdf": "pretty-xml", ".trig": "trig"}
-    dbis = {'turtle': '.ttl', 'n3': '.n3', 'json-ld': '.jsonld',
-        'xml': '.rdf', "pretty-xml" : '.rdf', 'nt': '.nt',
-       "trig": ".trig"}
-    
     # en l'absence de format, si le chemin comporte un
     # suffixe, on tente d'en déduire le format
     if not format and pfile.suffix:
-        format = d.get(pfile.suffix)
+        format = export_format_from_extension(pfile.suffix)
     if not format:
         format = 'turtle'
     
     # réciproquement, si le nom de fichier n'a pas
     # de suffixe, on en ajoute un d'après le format
     if not pfile.suffix:
-        pfile = pfile.with_suffix(dbis.get(format, ''))
+        pfile = pfile.with_suffix(
+            export_extension_from_format(format) or ''
+            )
     
     s = metagraph.serialize(
         format=format,
@@ -3305,7 +3298,130 @@ def export_metagraph(metagraph, shape, filepath, format=None):
     
     with open(pfile, 'wb') as dest:
         dest.write(s)
+
+
+def import_formats():
+    """Renvoie la liste de tous les formats disponibles pour l'import.
     
+    ARGUMENTS
+    ---------
+    Néant.
+    
+    RESULTAT
+    --------
+    Une liste (list) de formats (str).
+    """
+    return [ k for k, v in rdflib_formats.items() if v['import'] ]
+
+
+def export_formats():
+    """Renvoie la liste de tous les formats disponibles pour l'export.
+    
+    ARGUMENTS
+    ---------
+    Néant.
+    
+    RESULTAT
+    --------
+    Une liste (list) de formats (str).
+    """
+    return [ k for k in rdflib_formats.keys() ]
+
+
+def import_extensions_from_format(format=None):
+    """Renvoie la liste des extensions associées à un format d'import.
+    
+    ARGUMENTS
+    ---------
+    - [optionnel] format (str): un format d'import présumé inclus dans
+    la liste des formats reconnus par les fonctions de RDFLib
+    (rdflib_formats avec import=True).
+    
+    RESULTAT
+    --------
+    La liste de toutes les extensions associées au format considéré,
+    avec le point.
+    
+    Si format n'est pas renseigné, la fonction renvoie la liste
+    de toutes les extensions reconnues pour l'import.
+    
+    EXEMPLES
+    --------
+    >>> rdf_utils.import_extensions('xml')
+    ['.rdf', '.xml']
+    """
+    if not format:
+        l = []
+        for k, d in rdflib_formats.items():
+            if d['import']:
+                l += d['extensions']
+        return l
+    
+    d = rdflib_formats.get(format)
+    if d and d['import']:
+        return d['extensions']
+
+
+def export_extension_from_format(format):
+    """Renvoie l'extension utilisée pour les exports dans le format considéré.
+    
+    ARGUMENTS
+    ---------
+    - format (str): un format d'export présumé inclus dans la liste
+    des formats reconnus par les fonctions de RDFLib (rdflib_formats).
+    
+    RESULTAT
+    --------
+    L'extension (str) à utiliser pour le format considéré, avec le
+    point.
+    
+    EXEMPLES
+    --------
+    >>> rdf_utils.export_extension('pretty-xml')
+    '.rdf'
+    """
+    d = rdflib_formats.get(format)
+    if d:
+        return d['extensions'][0]
+
+
+def import_format_from_extension(extension):
+    """Renvoie le format d'import correspondant à l'extension.
+    
+    ARGUMENTS
+    ---------
+    - extension (str) : une extension (avec point).
+    
+    La fonction renvoie None si l'extension n'est pas
+    reconnue.
+    
+    RESULTAT
+    --------
+    Un nom de format (str).
+    """
+    for k, d in rdflib_formats.items():
+        if d['import'] and extension in d['extensions']:
+            return k
+
+
+def export_format_from_extension(extension):
+    """Renvoie le format d'export correspondant à l'extension.
+    
+    ARGUMENTS
+    ---------
+    - extension (str) : une extension (avec point).
+    
+    La fonction renvoie None si l'extension n'est pas
+    reconnue.
+    
+    RESULTAT
+    --------
+    Un nom de format (str).
+    """
+    for k, d in rdflib_formats.items():
+        if d['export default'] and extension in d['extensions']:
+            return k
+
    
 def available_formats(metagraph, shape):
     """List valid export formats for given metagraph.
@@ -3326,7 +3442,7 @@ def available_formats(metagraph, shape):
     for n, u in shape.namespace_manager.namespaces():
             metagraph.namespace_manager.bind(n, u, override=True, replace=True)
 
-    l = ["turtle", "json-ld", "xml", "n3", "nt", "pretty-xml", "trig"]
+    l = export_formats()
 
     # Une méthode plus gourmande pourrait consister à purement et simplement
     # tester toutes les sérialisations possibles et retourner celles qui
@@ -3339,14 +3455,16 @@ def available_formats(metagraph, shape):
     
         if str(p).startswith('urn:uuid:'):
             for f in ("xml", "pretty-xml"):
-                l.remove(f)            
+                if f in l:
+                    l.remove(f)            
             break
             
         # try:
             # split_uri(p)
         # except:
             # for f in ("xml", "pretty-xml"):
-                # l.remove(f)            
+                # if f in l:
+                    # l.remove(f)            
             # break
             
     return l
@@ -3381,23 +3499,17 @@ def metagraph_from_file(filepath, format=None):
     if not pfile.is_file():
         raise TypeError("{} is not a file.".format(filepath))
     
-    # extensions possibles et formats correspondants :
-    d = {".ttl": "turtle", ".n3": "n3", ".json": "json-ld",
-         ".jsonld": "json-ld", ".xml": "xml", ".nt": "nt",
-         ".rdf": "xml", ".trig": "trig"}
-    
-    if format and not format in ("turtle", "json-ld", "xml",
-        "n3", "nt", "trig"):
+    if format and not format in import_formats():
         raise ValueError("Format '{}' is not supported.".format(format))
     
     if not format:
     
-        if not pfile.suffix in d:
+        if not pfile.suffix in import_extensions_from_format():
             raise TypeError("Couldn't guess RDF format from file extension."\
                             "Please use format to declare it manually.")
                             
         else:
-            format = d[pfile.suffix]
+            format = import_format_from_extension(pfile.suffix)
             # NB : en théorie, la fonction parse de RDFLib est censée
             # pouvoir reconnaître le format d'après l'extension, mais à
             # ce jour elle n'identifie même pas toute la liste ci-avant.
@@ -4099,10 +4211,52 @@ def copy_metagraph(src_metagraph, old_metagraph=None):
     return mMetagraph
     
     
-    
-    
-    
 
 class ForbiddenOperation(Exception):
     pass
+
+
+
+# formats reconnus par les fonctions de RDFLib
+# si la clé 'import' vaut False, le format n'est
+# pas reconnu à l'import. si 'export default'
+# vaut True, il s'agit du format d'export privilégié
+# pour les extensions de la clé 'extensions'.
+rdflib_formats = {
+    'turtle': {
+        'extensions': ['.ttl'],
+        'import': True,
+        'export default': True
+        },
+    'n3': {
+        'extensions': ['.n3'],
+        'import': True,
+        'export default': True
+        },
+    'json-ld': {
+        'extensions': ['.jsonld', '.json'],
+        'import': True,
+        'export default': True
+        },
+    'xml': {
+        'extensions': ['.rdf', '.xml'],
+        'import': True,
+        'export default': False
+        },
+    'pretty-xml': {
+        'extensions': ['.rdf', '.xml'],
+        'import': False,
+        'export default': True
+        },
+    'nt': {
+        'extensions': ['.nt'],
+        'import': True,
+        'export default': True
+        },
+    'trig': {
+        'extensions': ['.trig'],
+        'import': True,
+        'export default': True
+        }
+    }
 
