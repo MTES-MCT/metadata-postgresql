222304426942d3d6469ef23515324ad5e4b5fd05
alhyss <55992003+alhyss@users.noreply.github.com>
2021-12-02 23:57:32 +0100

    Update actionsbook.py

--- plume/rdf/actionsbook.py
+++ plume/rdf/actionsbook.py
@@ -27,39 +27,72 @@ class ActionsBook:
     
     Attributes
     ----------
-    show : WidgetKey
-        Clés dont les widgets doivent être rendus visibles.
-    show_minus_button : WidgetKey
-        Clés dont le bouton moins doit être rendu visible,
+    show : VisibleKeyList
+        Liste des clés dont les widgets doivent être rendus visibles.
+    show_minus_button : VisibleKeyList
+        Liste des clés dont le bouton moins doit être rendu visible,
         s'il existe.
-    hide : WidgetKey
-        Clés dont les widgets doivent être masqués.
-    hide_minus_button : WidgetKey
-        Clés dont le bouton moins doit être masqué, s'il existe.
-    drop : WidgetKey
-        Clés dont les widgets doivent être supprimés.
-    create : WidgetKey
-        Clés dont les widgets doivent être créés.
-    move : WidgetKey
-        Clés dont les widgets doivent être déplacés dans la grille.
-    languages : WidgetKey
-        Clés dont le menu des langues doit être mis à jour.
-    sources : WidgetKey
-        Clés dont le menu des sources doit être mis à jour.
-    thesaurus : WidgetKey
-        Clés dont la liste de valeurs doit être recalculée.
+    hide : NoGhostKeyList
+        Liste des clés dont les widgets doivent être masqués.
+    hide_minus_button : NoGhostKeyList
+        Liste des clés dont le bouton moins doit être masqué, s'il existe.
+    drop : NoGhostKeyList
+        Liste des clés dont les widgets doivent être supprimés.
+    create : NoGhostKeyList
+        Liste des clés dont les widgets doivent être créés.
+    move : NoGhostKeyList
+        Liste des clés dont les widgets doivent être déplacés dans la grille.
+    languages : NoGhostKeyList
+        Liste des clés dont le menu des langues doit être mis à jour.
+    sources : NoGhostKeyList
+        Liste des clés dont le menu des sources doit être mis à jour.
+    thesaurus : NoGhostKeyList
+        Liste des clés dont la liste de valeurs doit être recalculée.
     
     """
     
     def __init__(self):
-        self.show = []
-        self.show_minus_button = []
-        self.hide = []
-        self.hide_minus_button = []
-        self.drop = []
-        self.create = []
-        self.move = []
-        self.languages = []
-        self.source = []
-        self.thesaurus = []
+        self.show = VisibleKeyList()
+        self.show_minus_button = VisibleKeyList()
+        self.hide = NoGhostKeyList()
+        self.hide_minus_button = NoGhostKeyList()
+        self.drop = NoGhostKeyList()
+        self.create = NoGhostKeyList()
+        self.move = NoGhostKeyList()
+        self.languages = NoGhostKeyList()
+        self.source = NoGhostKeyList()
+        self.thesaurus = NoGhostKeyList()
+
+class NoGhostKeyList(list):
+    """Liste de clés garantie sans fantôme.
+    
+    Notes
+    -----
+    Cette classe réécrit la méthode `append` de `list` pour
+    exclure silencieusement les clés fantômes. Les clés en
+    cours d'initialisation ne sont pas non plus prises en
+    compte, pour éviter que la même clé apparaisse dans
+    `create`, `show`, `move`...
+    
+    """
+    def append(self, value):
+        if not value.is_ghost and not value._is_unborn:
+            super().append(value)
+
+class VisibleKeyList(list):
+    """Liste de clés garanties visibles.
+    
+    Notes
+    -----
+    Cette classe réécrit la méthode `append` de `list` pour
+    exclure silencieusement les clés non visibles. Les clés en
+    cours d'initialisation ne sont pas non plus prises en
+    compte, pour éviter que la même clé apparaisse dans
+    `create`, `show`, `move`...
+    
+    """
+    def append(self, value):
+        if not value.is_ghost and not value.is_hidden_b and \
+            not value.is_hidden_m and not value._is_unborn:
+            super().append(value)
 
