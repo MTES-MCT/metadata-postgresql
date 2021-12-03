efdabe38106aeee45e6debee5ef9b3d674715b19
alhyss <55992003+alhyss@users.noreply.github.com>
2021-12-03 23:41:09 +0100

    Update actionsbook_test.py

--- plume/rdf/tests/actionsbook_test.py
+++ plume/rdf/tests/actionsbook_test.py
@@ -5,7 +5,7 @@ from rdflib import URIRef, DC, RDFS
 
 from plume.rdf.actionsbook import ActionsBook
 from plume.rdf.widgetkey import RootKey, ObjectKey, GroupOfPropertiesKey, \
-     TranslationGroupKey, ValueKey, WidgetKey
+     TranslationGroupKey, ValueKey, WidgetKey, TranslationButtonKey
 
 class ActionsBookTestCase(unittest.TestCase):
 
@@ -16,7 +16,7 @@ class ActionsBookTestCase(unittest.TestCase):
         r = RootKey()
         g = ObjectKey(parent=r, is_ghost=True, predicate=DC.title)
         w = ObjectKey(parent=r, predicate=DC.description)
-        a = r.unload_actionsbook()
+        a = WidgetKey.unload_actionsbook()
         self.assertEqual(a.create, [r, w])
 
     def test_visible(self):
@@ -24,21 +24,43 @@ class ActionsBookTestCase(unittest.TestCase):
         
         """
         r = RootKey()
-        WidgetKey.langlist = ['fr']
-        WidgetKey.main_language = 'fr'
+        WidgetKey.langlist = ['fr', 'en']
         g = GroupOfPropertiesKey(parent=r,
             rdftype=URIRef('http://purl.org/dc/terms/RightsStatement'),
             predicate=URIRef('http://purl.org/dc/terms/accessRights'))
-        m = ValueKey(parent=r, m_twin=g)
+        m = ValueKey(parent=r, m_twin=g, is_hidden_m=False)
         t = TranslationGroupKey(parent=g, predicate=RDFS.label)
-        w = ValueKey(parent=t)
-        WidgetKey.clear_actionsbook()
-        w.kill()
-        a = r.unload_actionsbook()
+        b = TranslationButtonKey(parent=t)
+        self.assertFalse(b.is_hidden_b)
+        w1 = ValueKey(parent=t)
+        w2 = ValueKey(parent=t)
+        self.assertTrue(b.is_hidden_b)
+        self.assertFalse(w1.is_single_child)
+        a = WidgetKey.unload_actionsbook()
+        self.assertEqual(a.show_minus_button, [])
+        # rien, car même si w1 n'est plus un
+        # enfant unique, il appartient à
+        # une branche masquée
+        w2.kill()
+        self.assertFalse(b.is_hidden_b)
+        a = WidgetKey.unload_actionsbook()
         self.assertEqual(a.show, [])
-        m.hidden_m = True
-        a = r.unload_actionsbook()
-        self.assertEqual(a.show, [w])
+        # rien, car même si une langue est de
+        # nouveau disponible, le bouton appartient
+        # à une branche masquée
+        m.is_hidden_m = True
+        a = WidgetKey.unload_actionsbook()
+        self.assertEqual(a.show, [g, t, w1, b])
+        w2 = ValueKey(parent=t)
+        self.assertFalse(w1.is_single_child)
+        a = WidgetKey.unload_actionsbook()
+        self.assertEqual(a.show_minus_button, [w1])
+        m.is_hidden_m = False
+        WidgetKey.clear_actionsbook()
+        m.is_hidden_m = True
+        a = WidgetKey.unload_actionsbook()
+        self.assertEqual(a.show, [g, t, w1, w2])
+        
         
     
 unittest.main()
