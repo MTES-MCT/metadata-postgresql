f8534c8c3dcfc975e9b8595613e352990dc4fa92
alhyss <55992003+alhyss@users.noreply.github.com>
2021-12-07 22:32:22 +0100

    Update actionsbook.py

--- plume/rdf/actionsbook.py
+++ plume/rdf/actionsbook.py
@@ -13,12 +13,9 @@ Pour faciliter leur exploitation, les `ActionsBook` sont
 traduits en dictionnaires par la méthode `actionsbook_to_dict`
 de la classe `WidgetsDict`.
 
-Les listes des carnets d'actions contiennent par construction
-des informations inutiles (comme le fait de rendre visible le
-bouton moins annexé à un widget qui n'en a pas) ou redondantes
-(comme le fait de déplacer un widget qui n'a pas encore été créé).
-La consolidation est également du ressort de la méthode
-`actionsbook_to_dict`.
+Les carnets d'actions sont supposés être lus et réinitialisés
+après chaque commande, sans quoi ils finiront immanquablement par
+contenir des informations contradictoires ou fausses.
 
 """
 
@@ -27,92 +24,116 @@ class ActionsBook:
     
     Attributes
     ----------
-    show : VisibleKeyList
+    show : list
         Liste des clés dont les widgets doivent être rendus visibles.
-    show_minus_button : TrueMinusButtonKeyList
+    show_minus_button : list
         Liste des clés dont le bouton moins doit être rendu visible,
         s'il existe.
-    hide : NoGhostKeyList
+    hide : list
         Liste des clés dont les widgets doivent être masqués.
-    hide_minus_button : TrueMinusButtonKeyList
+    hide_minus_button : list
         Liste des clés dont le bouton moins doit être masqué, s'il existe.
-    drop : NoGhostKeyList
-        Liste des clés dont les widgets doivent être supprimés.
-    create : NoGhostKeyList
+    create : list
         Liste des clés dont les widgets doivent être créés.
-    move : NoGhostKeyList
+    move : list
         Liste des clés dont les widgets doivent être déplacés dans la grille.
-    languages : NoGhostKeyList
+    languages : list
         Liste des clés dont le menu des langues doit être mis à jour.
-    sources : NoGhostKeyList
+    sources : list
         Liste des clés dont le menu des sources doit être mis à jour.
-    thesaurus : NoGhostKeyList
+    thesaurus : list
         Liste des clés dont la liste de valeurs doit être recalculée.
+    drop : list
+        Liste des clés dont les widgets doivent être supprimés.
     
     """
     
     def __init__(self):
-        self.show = VisibleKeyList()
-        self.show_minus_button = TrueMinusButtonKeyList()
-        self.hide = NoGhostKeyList()
-        self.hide_minus_button = TrueMinusButtonKeyList()
-        self.drop = NoGhostKeyList()
-        self.create = NoGhostKeyList()
-        self.move = NoGhostKeyList()
-        self.languages = NoGhostKeyList()
-        self.source = NoGhostKeyList()
-        self.thesaurus = NoGhostKeyList()
+        self.show = VisibleKeyList(actionsbook=self, erase=['hide',
+            'show_minus_button'])
+        self.show_minus_button = TrueMinusButtonKeyList(actionsbook=self,
+            erase=['hide_minus_button'])
+        self.hide = NoGhostKeyList(actionsbook=self, erase=['show',
+            'show_minus_button', 'hide_minus_button'])
+        self.hide_minus_button = TrueMinusButtonKeyList(actionsbook=self,
+            erase=['show_minus_button'])
+        self.create = NoGhostKeyList(actionsbook=self, erase=['show',
+            'show_minus_button', 'hide', 'hide_minus_button', 'move',
+            'languages', 'sources', 'thesaurus'])
+        self.move = NoGhostKeyList(actionsbook=self)
+        self.languages = NoGhostKeyList(actionsbook=self)
+        self.sources = NoGhostKeyList(actionsbook=self)
+        self.thesaurus = NoGhostKeyList(actionsbook=self)
+        self.drop = NoGhostKeyList(actionsbook=self, erase=['show',
+            'show_minus_button', 'hide', 'hide_minus_button', 'create',
+            'move', 'languages', 'sources', 'thesaurus'])
+
+    def __bool__(self):
+        return sum(len(getattr(self, a)) for a in self.__dict__.keys()) > 0
+
 
 class NoGhostKeyList(list):
     """Liste de clés garantie sans fantôme.
+    
+    Parameters
+    ----------
+    actionsbook : ActionsBook
+        Le carnet d'actions auquel appartient la liste.
+    erase : list of str, optional
+        La liste des attributs où la clé doit être supprimée
+        dès lors qu'elle apparaît dans la présente liste.
+    
+    Attributes
+    ----------
+    actionsbook : ActionsBook
+        Le carnet d'actions auquel appartient la liste. Cet
+        attribut sert à croiser les listes pour veiller
+        à leur cohérence.
+    erase : list of str
+        La liste des attributs où la clé doit être supprimée
+        dès lors qu'elle apparaît dans la présente liste.
     
     Notes
     -----
     Cette classe réécrit la méthode `append` de `list` pour
     exclure silencieusement les clés fantômes. Les clés en
     cours d'initialisation ne sont pas non plus prises en
-    compte, pour éviter que la même clé apparaisse dans
-    `create`, `show`, `move`...
+    compte, de même que les clés qui se trouvent déjà dans
+    `create`.
     
     """
+    def __init__(self, actionsbook, erase=None):
+        self.actionsbook = actionsbook
+        self.erase = erase or []
+        super().__init__(self)
+    
     def append(self, value):
         if value and not value._is_unborn:
-            super().append(value)
+            if not value in self and \
+                not value in self.actionsbook.create:
+                super().append(value)
+            for a in self.erase:
+                l = getattr(self.actionsbook, a)
+                if value in l:
+                    l.remove(value)
 
-class VisibleKeyList(list):
+class VisibleKeyList(NoGhostKeyList):
     """Liste de clés garanties visibles.
-    
-    Notes
-    -----
-    Cette classe réécrit la méthode `append` de `list` pour
-    exclure silencieusement les clés non visibles. Les clés en
-    cours d'initialisation ne sont pas non plus prises en
-    compte, pour éviter que la même clé apparaisse dans
-    `create`, `show`, `move`...
     
     """
     def append(self, value):
-        if not value.is_hidden and not value._is_unborn:
+        if not value.is_hidden:
             super().append(value)
 
-class TrueMinusButtonKeyList(list):
+class TrueMinusButtonKeyList(VisibleKeyList):
     """Liste de clés garanties visibles et avec un bouton moins.
     
     Ceci ne présage pas de la visibilité du bouton moins
     lui-même.
     
-    Notes
-    -----
-    Cette classe réécrit la méthode `append` de `list` pour
-    exclure silencieusement les clés non visibles ou sans
-    bouton moins. Les clés en cours d'initialisation ne sont
-    pas non plus prises en compte, pour éviter que la même
-    clé apparaisse dans `create`, `show`, `move`...
-    
     """
     def append(self, value):
-        if not value.is_hidden and not value._is_unborn \
-            and value.has_minus_button:
+        if value.has_minus_button:
             super().append(value)
 
 
