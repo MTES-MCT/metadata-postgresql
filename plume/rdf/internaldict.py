2de0274d55a33e3a2761783c5c0e19b49c9cc1aa
alhyss <55992003+alhyss@users.noreply.github.com>
2021-12-18 23:52:29 +0100

    Update internaldict.py
    Reprise du docstring. Modification de l'ordre des clés et suppression de `is_hidden_m` (`hidden` reprenant désormais l'attribut `is_hidden` de la clé du dictionnaire de widgets).

--- plume/rdf/internaldict.py
+++ plume/rdf/internaldict.py
@@ -3,151 +3,128 @@
 """
 
 class InternalDict(dict):
-    """Classe pour les dictionnaires internes.
+    """Dictionnaires interne.
     
-    Cette classe n'a pas d'attribut.
+    Un dictionnaire interne est un dictionnaire comportant les clés listées ci-après. Les
+    valeurs des dictionnaires de widgets (:py:class:`plume.rdf.widgetsdict.WidgetsDict`) sont
+    des dictionnaires internes. Un dictionnaire interne traduit les attributs des clés
+    du dictionnaire de widgets sous une forme plus aisément exploitable pour construire un
+    formulaire avec les bibliothèques QT. Il a également pour fonction de référencer les widgets
+    et autres objets QT qui constituent le formulaire, afin de pouvoir les modifier d'autant que
+    de besoin à mesure des actions de l'utilisateur.
     
-    Un objet de classe `InternalDict`, ou "dictionnaire interne" est un dictionnaire
-    comportant les clés listées ci-après. Les valeurs des dictionnaires de widgets
-    (`WidgetsDict`) sont des dictionnaires internes.
-    
-    - 'object' : classification des éléments du dictionnaire. "group of values" ou
-    "group of properties" ou "translation group" ou "edit" ou "plus button" ou "translation button".
-    Les "translation group" et "translation button" ne peuvent exister que si l'attribut
-    "translation" vaut True.
+    À l'initialisation, toutes les clés du dictionnaire interne ont pour valeur ``None``.
+    Elles doivent être calculées avant la création des widgets avec la méthode
+    :py:meth:`plume.rdf.widgetsdict.WidgetsDict.internalize`, et recalculées par le
+    même moyen à chaque fois que la clé du dictionnaire de widgets qui porte le dictionnaire
+    interne est modifiée.
+        
+    * ``object`` : classification par nature des éléments du dictionnaire. L'objet est une
+      chaîne de caractères pouvant prendre les valeurs :
+      * ``'group of values'`` pour un groupe de valeurs,
+      * ``'group of properties'`` pour un groupe de propriétés,
+      * ``'translation group'`` pour un groupe de traduction,
+      * ``'edit'`` pour un widget de saisie,
+      * ``'plus button'`` pour un bouton plus,
+      * ``'translation button'`` pour un bouton de traduction.
+    * ``main widget type`` est le type du widget principal de la clé (``'QGroupBox'``,
+      ``'QLineEdit'``...). Si ``None`` (clé dite "fantôme), aucun widget ne doit être créé.
+      Ce cas correspond aux catégories de métadonnées dont le modèle couramment utilisé ne
+      prévoit pas l'affichage, mais dont il n'est pas question de perdre les valeurs.
 
-    Les clés 'X widget' ci-après ont vocation à accueillir les futurs widgets (qui ne sont pas
-    créés par la fonction). Il y a toujours un widget principal, le plus souvent de type
-    QGroupBox ou QXEdit, et éventuellement des widgets secondaires. Tous ont le même parent et
-    seraient à afficher sur la même ligne de la grille.
+    Les clés ``... widget`` ci-après ont vocation à accueillir les futurs widgets.
     
-    - 'main widget' : widget principal.
-    - 'grid widget' : widget de type QGridLayout, à créer pour tous les éléments "group of values",
-    "group of properties" et "translation group".
-    - 'label widget' : pour certains éléments "edit" (concrètement, tous ceux dont
-    la clé "label" ci-après n'est pas vide - pour les autres il y a un "group of values" qui porte
-    le label), le widget QLabel associé.
-    - 'minus widget' : pour les propriétés qui admettent plusieurs valeurs ou qui, de fait, en ont,
-    (concrètement celles pour lesquelles la clé "has minus button" vaut True), widget QButtonTool [-]
-    permettant de supprimer les widgets.
-    - 'language widget' : pour certains widgets "edit" (concrètement, ceux dont 'authorized languages'
-    n'est pas vide), widget pour afficher/sélectionner la langue de la métadonnée. Les 'languages
-    widget' n'auront vocation à être affiché que si le mode "traduction" est actif, soit un 
-    paramètre 'translation' valant True pour la fonction.
-    - 'switch source widget' : certaines catégories de métadonnées peuvent prendre une valeur
-    parmi plusieurs thésaurus ou permettre de choisir entre une valeur issue d'un thésaurus et
-    une valeur saisie manuellement. Le bouton "switch source widget" est un QButtonTool qui permet
-    de sélectionner le thésaurus à utiliser ou de basculer en mode manuel. Il doit être implémenté
-    dès lors que la clé 'multiple sources' indique True. Cf. aussi 'sources' et 'current sources'.
+    * ``main widget`` : widget principal.
+    * ``grid widget`` : widget annexe de type ``QGridLayout``, à créer dès lors que le widget
+      principal est de type ``'QGroupBox'``.
+    * ``label widget`` : widget annexe de type ``QLabel`` portant une étiquette qui fournit le
+      libellé de la catégorie de métadonnée. À créer lorsque la valeur de ``has label`` est
+      ``True``. Le libellé à afficher est la valeur de ``label``.
+    * ``minus widget`` :  widget annexe de type ``QButtonTool`` permettant à l'utilisateur
+      de supprimer le widget. À créer lorsque la valeur de ``has minus button`` est ``True``.
+    * ``language widget`` : widget annexe de type ``QButtonTool`` permettant à l'utilisateur
+      de spécifier la langue du texte qu'il saisit. À créer lorsque la valeur de
+      ``authorized languages`` n'est pas ``None``.
+    * ``switch source widget`` : widget annexe de type ``QButtonTool`` permettant à l'utilisateur
+      de spécifier le thésaurus qu'il veut utiliser, lorsque plusieurs sont disponibles, ou
+      de passer en mode manuel (saisie manuelle des propriétés de l'objet, au lieu de la simple
+      sélection de son IRI dans un thésaurus). À créer lorsque la valeur de ``multiple sources``
+      est ``True``.
     
-    Parmi les trois derniers boutons seuls deux au maximum pourront être affichés simultanément,
-    la combinaison 'language widget' et 'switch source widget' étant impossible.
+    En complément, des clés sont prévues pour les ``QMenu`` et ``QAction`` associés à certains
+    widgets boutons.
     
-    En complément, des clés sont prévues pour les QMenu et QAction associés à certains widgets.
-    
-    - 'switch source menu' : pour le QMenu associé au 'switch source widget'.
-    - 'switch source actions' : liste des QAction associées au 'switch source menu'.
-    - 'language menu' : pour le QMenu associé au 'language widget'.
-    - 'language actions' : liste des QAction associées au 'language menu'.   
+    * ``switch source menu`` : pour le ``QMenu`` associé au widget référencé par 
+      ``switch source widget``. Ce menu présente une liste de sources (cf. ``sources``),
+      dont une est sélectionnée (cf. ``current source``).
+    * ``switch source actions`` : liste des ``QAction`` du ``switch source menu``.
+    * ``language menu`` : pour le ``QMenu`` associé au widget référencé par
+      ``language widget``. Ce menu présente une liste de langues (cf. ``authorized languages``),
+      dont une est sélectionnée (cf. ``language value``), il permet à l'utilisateur de
+      spécifier la langue du texte qu'il est en train de saisir.
+    * ``language actions`` : liste des ``QAction`` du ``language menu``.   
 
-    Les clés suivantes sont, elles, remplies par la fonction, avec toutes les informations nécessaires
-    au paramétrage des widgets.
+    Les clés suivantes contiennent les informations nécessaires au paramétrage des widgets.
     
-    - 'main widget type'* : type du widget principal (QGroupBox, QLineEdit...). Si cette clé est
-    vide, c'est qu'aucun widget ne doit être créé. Ce cas correspond aux catégories de métadonnées
-    hors template, dont les valeurs ne seront pas affichées, mais qu'il n'est pas question d'effacer
-    pour autant.
-    - 'row' : placement vertical (= numéro de ligne) du widget dans la grille (QGridLayout) qui
-    organise tous les widgets rattachés au groupe parent (QGroupBox). Initialement, row est
-    toujours égal à l'index de la clé, mais il ne le sera plus si les boutons d'ajout/suppression
-    de valeurs ont été utilisés.
-    - 'row span'* : hauteur du widget, en nombre de lignes de la grille. Spécifié uniquement pour
-    certains QTextEdit (valeur par défaut à prévoir).
-    - 'label'* : s'il y a lieu de mettre un label (pour les "group of values" et les "edit" ou "node
-    group" qui n'ont pas de "group of values" parent), label intégré au widget QGroupBox ou, pour un
-    widget "edit", le label du QLabel associé.
-    - 'label row' : pour les widgets QTextEdit ou lorsque le label dépasse la longueur définie par
-    l'argument labelLengthLimit, il est considéré que le label sera affiché au-dessus de la zone
-    de saisie. Dans ce cas 'label row' contient la valeur à donner au paramètre row. Si cette clé
-    est vide alors que 'label' contient une valeur, c'est que le label doit être positionné sur
-    la même ligne que le widget principal, et son paramètre row est donc fourni par l'index
-    contenu dans la clé du dictionnaire externe.
-    - 'help text'* : s'il y a lieu, texte explicatif sur la métadonnée. Pourrait être affiché en
-    infobulle.
-    - 'value' : pour un widget "edit", la valeur à afficher. Elle tient compte à la fois de ce qui
-    était déjà renseigné dans la fiche de métadonnées et des éventuelles valeurs par défaut de shape
-    et template. Les valeurs par défaut sont utilisées uniquement dans les groupes de propriétés
-    vides.
-    - 'placeholder text'* : pour un widget QLineEdit ou QTextEdit, éventuel texte à utiliser comme
-    "placeholder".
-    - 'input mask'* : éventuel masque de saisie.
-    - 'language value' : s'il y a lieu de créer un widget secondaire pour spécifier la langue (cf.
-    'language widget'), la langue à afficher dans ce widget. Elle est déduite de la valeur lorsqu'il
-    y a une valeur, sinon on utilise la valeur de l'argument language de la fonction.
-    - 'authorized languages' : liste des langues disponibles dans le 'language widget', s'il y a
-    lieu (cf. 'language widget').
-    - 'is mandatory'* : booléen indiquant si la métadonnée doit obligatoirement être renseignée.
-    - 'has minus button' : booléen indiquant si un bouton de suppression du widget doit être
-    créé (cf. minus widget). Sur le principe, ce sera le cas dès qu'une catégorie admet plusieurs
-    valeurs ou traductions, ou qu'il y a effectivement plusieurs valeurs saisies (un bouton pour
-    chaque valeur).
-    - 'hide minus button' : booléen qui vaudra True si le bouton moins doit être masqué parce
-    qu'il ne reste qu'un widget dans le groupe de valeurs ou groupe de traduction.
-    - 'regex validator pattern' : pour un widget "edit", une éventuelle expression régulière que la
-    valeur est censée vérifier. Pour usage par un QRegularExpressionValidator.
-    - 'regex validator flags' : flags associés à 'regex validator pattern', le cas échéant.
-    - 'type validator' : si un validateur basé sur le type (QIntValidator ou QDoubleValidator) doit
-    être utilisé, le nom du validateur.
-    - 'sources' : lorsque la métadonnée prend ses valeurs dans une ou plusieurs ontologies /
-    thésaurus, liste des sources. La valeur 'manuel' indique qu'une saisie manuelle est possible
-    (uniquement via un widget "M").
-    - 'multiple sources' : booléen indiquant que la métadonnée fait appel à plusieurs ontologies
-    ou autorise à la fois la saisie manuelle et l'usage d'ontologies.
-    - 'current source' spécifie la source en cours d'utilisation. Pour un widget "M", vaut
-    toujours "manuel" lorsque le widget est en cours d'utilisation, None sinon). Pour un widget
-    non "M", il donne le nom littéral de l'ontologie ou "non répertorié" lorsque la valeur
-    antérieurement saisie n'apparaît dans aucune ontologie associée à la catégorie, None si le
-    widget n'est pas utilisé. La liste des termes autorisés par la source n'est pas directement
-    stockée dans le dictionnaire, mais peut être obtenue via la fonction build_vocabulary.
-    - 'thesaurus values' : le cas échéant, la liste des valeurs à afficher dans le QComboBox.
-    - 'read only'* : booléen qui vaudra True si la métadonnée ne doit pas être modifiable par
-    l'utilisateur. En mode lecture, 'read only' vaut toujours True.
-    - 'hidden' : booléen. Si True, le widget principal doit être masqué. Concerne
-    les boutons de traduction, lorsque toutes les langues disponibles (cf. langList) ont été
-    utilisées.
-    - 'hidden M' : booléen. Si True, le widget principal doit être masqué. Concerne les branches
-    M/non M (qui permettent de saisir une métadonnées soit sous forme d'IRI soit sous forme d'un
-    ensemble de propriétés littérales) lorsque l'autre forme est utilisée.
-
-    La dernière série de clés ne sert qu'aux fonctions de rdf_utils : 'default value'* (valeur par
-    défaut), 'node kind', 'data type'**, 'class', 'path'***, 'subject', 'predicate', 'node',
-    'transform', 'default widget type', 'one per language', 'next child' (indice à utiliser si un
-    enregistrement est ajouté au groupe), 'multiple values'* (la catégorie est-elle
-    censée admettre plusieurs valeurs ?), 'order shape', 'order template'* (ordre des catégories, cette
-    clé s'appelle simplement "order" dans le template), 'do not save', 'sources URI', 'default source',
-    'independant label'.
-
-    * ces clés apparaissent aussi dans le dictionnaire interne de template.
-    ** le dictionnaire interne de template contient également une clé 'data type', mais dont les
-    valeurs sont des chaînes de catactères parmi 'string', 'boolean', 'decimal', 'integer', 'date',
-    'time', 'dateTime', 'duration', 'float', 'double' (et non des objets de type URIRef).
-    *** le chemin est la clé principale de template.
+    * ``hidden`` : booléen. Si ``True``, le widget principal et tous les widgets annexes doivent
+      être masqués.
+    * ``label`` : s'il y a lieu de mettre une étiquette, soit intégrée au widget (par exemple
+      dans le cas d'un ``QGroupBox``), soit indépendante (cf. ``has label``), le libellé
+      que portera ladite étiquette.
+    * ``has label`` : ``True`` si un widget ``QLabel`` doit être créé pour porter le libellé
+      de la catégorie de métadonnée. Il devra alors être référencé dans ``label widget``.
+    * ``help text`` : le cas échéant, texte explicatif à afficher en infobulle.
+    * ``value`` : pour un widget de saisie, l'éventuelle valeur à afficher.
+    * ``placeholder text`` : le cas échéant, valeur du paramètre ``placeholder`` (texte de
+      substitution, qui apparaît tant qu'aucune valeur n'est renseignée) pour le widget principal.
+    * ``input mask`` : le cas échéant, valeur du paramètre ``inputMask`` (masque de saisie)
+      pour le widget principal.
+    * ``is mandatory`` : ``True`` si la métadonnée doit obligatoirement être renseignée.
+    * ``read only`` : ``True`` si la métadonnée ne doit pas être modifiable par l'utilisateur,
+      ce qui conduira à désactiver le widget de saisie.
+    * ``has minus button`` : ``True`` si un widget annexe bouton moins doit être créé. Il sera
+      alors référencé dans ``minus widget``.
+    * ``hide minus button`` : ``True`` si le widget annexe bouton moins doit être masqué. À noter
+      que même si la valeur de cette clé n'est pas ``True``, le bouton moins devra être masqué
+      si ``hidden`` vaut ``True``.
+    * ``regex validator pattern`` : le cas échéant, éventuelle expression régulière que la
+      valeur est censée vérifier. Pour usage par un ``QRegularExpressionValidator``.
+    * ``regex validator flags`` : flags associés à ``regex validator pattern``, le cas échéant.
+    * ``type validator`` : si un validateur basé sur le type (``QIntValidator`` ou
+      ``QDoubleValidator``) doit être utilisé, le nom du validateur.
+    * ``multiple sources`` : ``True`` si la métadonnée fait appel à plusieurs thésaurus
+      ou autorise à la fois la saisie manuelle et l'usage de thésaurus. Un widget de sélection
+      de la source doit alors être créé et référencé dans ``switch source widget``.
+    * ``sources`` : s'il y a lieu de créer un widget annexe pour spécifier la source
+      (cf. ``multiple sources``), la liste des sources à afficher dans le menu associé
+      (``switch source menu``).
+    * ``current source`` : s'il y a lieu de créer un widget annexe pour spécifier la source
+      (cf. ``multiple sources``), la source qui devra être présentée comme actuellement
+      sélectionnée par le menu associé (``switch source menu``).
+    * ``thesaurus values`` : le cas échéant, la liste des valeurs à afficher dans le
+      widget principal de type ``QComboBox``.
+    * ``authorized languages`` : liste des langues à présenter dans le menu associé au
+       widget de sélection de la langue (``language menu``), le cas échéant. La non nullité
+       de cette clé emporte la création d'un widget de sélection de la langue (``language widget``).
+    * ``language value`` : s'il y a lieu de créer un widget annexe pour spécifier la langue
+      (cf. ``authorized languages``), la langue qui devra être présentée comme actuellement
+      sélectionnée par le menu associé (``language menu``).
+    
     """
     
     def __init__(self):
         keys = [
-            'object',
+            'object', 'main widget type',
             # stockage des widgets :
             'main widget', 'grid widget', 'label widget', 'minus widget',
             'language widget', 'switch source widget', 'switch source menu',
             'switch source actions', 'language menu', 'language actions',
-            # paramétrage des widgets
-            'main widget type', 'label', 'help text', 'value', 'language value',
-            'placeholder text', 'input mask', 'is mandatory', 'has minus button',
+            # paramétrage des widgets :
+            'hidden', 'label', 'has label', 'help text', 'value', 'placeholder text',
+            'input mask', 'is mandatory', 'read only', 'has minus button',
             'hide minus button', 'regex validator pattern', 'regex validator flags',
             'type validator', 'multiple sources', 'sources', 'current source',
-            'thesaurus values', 'authorized languages', 'read only', 'hidden',
-            'hidden M',
+            'thesaurus values', 'authorized languages', 'language value'
             ]
         self.update({ k:None for k in keys })
 
