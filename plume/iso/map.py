46e70fca1b1fd317b93ee6edd0469e5713122ca0
alhyss <55992003+alhyss@users.noreply.github.com>
2021-12-24 20:17:03 +0100

    Update map.py

--- plume/iso/map.py
+++ plume/iso/map.py
@@ -5,10 +5,12 @@
 from json import load
 import xml.etree.ElementTree as etree
 
-from plume.rdf.utils import DatasetId, abspath, forbidden_char
+from plume.rdf.utils import DatasetId, abspath, forbidden_char, \
+    owlthing_from_email, owlthing_from_tel
 from plume.rdf.namespaces import DCAT, DCT, FOAF, OWL, RDF, \
-    RDFS, SKOS, VCARD, XSD, PROV
+    RDFS, SKOS, VCARD, XSD, GEODCAT
 from plume.rdf.rdflib import URIRef, Literal, BNode
+from plume.rdf.thesaurus import Thesaurus
 
 
 ns = {
@@ -51,9 +53,9 @@ class IsoToDcat:
         requête sur le CSW ne renvoie pas de résultat). Idem
         si la dé-sérialisation du XML a échoué pour une raison
         ou une autre.
-    triples : list
-        Les triples résultant de la conversation des métadonnées
-        selon GeoDCAT-AP.
+    triples : list of tuples
+        Liste des triples résultant de la conversation des
+        métadonnées selon GeoDCAT-AP.
     datasetid : plume.rdf.utils.DatasetId
         L'identifiant du jeu de données.
     language : str
@@ -134,25 +136,117 @@ class IsoToDcat:
             self.datasetid, DCT.description, language=self.language)
 
     @property
+    def map_epsg(self):
+        epsg = self.isoxml.findtext('./gmd:referenceSystemInfo/'
+            'gmd:MD_ReferenceSystem/gmd:referenceSystemIdentifier/'
+            'gmd:RS_Identifier/gmd:code/gmx:Anchor',
+            namespaces=ns)
+        if not epsg:
+            epsg_txt = self.isoxml.findtext('./gmd:referenceSystemInfo/'
+                'gmd:MD_ReferenceSystem/gmd:referenceSystemIdentifier/'
+                'gmd:RS_Identifier/gmd:code/gco:CharacterString',
+                namespaces=ns)
+            r = re.search('EPSG:([0-9]*)', epsg_txt)
+            if r:
+                epsg = r[1]
+        if not epsg:
+            return []
+        node = BNode()
+        return [(self.datasetid, DCT.conformsTo, node),
+            (node, SKOS.inScheme, URIRef('http://www.opengis.net/def/crs/EPSG/0')),
+            (node, DCT.identifier, Literal(epsg))]
+
+    @property
+    def map_dates(self):
+        l = []
+        type_map = {
+            'creation': DCT.created,
+            'revision': DCT.issued,
+            'publication': DCT.modified
+            }
+        for elem in self.isoxml.findall('./gmd:identificationInfo/'
+            'gmd:MD_DataIdentification/gmd:citation/gmd:CI_Citation/'
+            'gmd:date', namespaces=ns):
+            c = elem.find('./gmd:CI_Date/gmd:dateType/'
+                'gmd:CI_DateTypeCode', namespaces=ns)
+            if c is None:
+                continue
+            code = c.text or c.get('codeListValue')
+            if not code or not code in type_map:
+                continue
+            l += find_literal(elem, './gmd:CI_Date/gmd:date/'
+                'gco:Date', self.datasetid, type_map[code],
+                datatype=XSD.date)
+        return l
+
+    @property
+    def map_keywords(self):
+        l = []
+        for elem in self.isoxml.findall('./gmd:identificationInfo/'
+            'gmd:MD_DataIdentification/gmd:descriptiveKeywords',
+            namespaces=ns):
+            t = elem.findtext('./gmd:MD_Keywords/gmd:thesaurusName/'
+                'gmd:CI_Citation/gmd:title/gco:CharacterString',
+                namespaces=ns)
+            keyword = elem.findtext('./gmd:MD_Keywords/gmd:keyword/'
+                'gco:CharacterString', namespaces=ns)
+            if t and 'INSPIRE themes' in t:
+                keyword_iri = Thesaurus.concept_iri((
+                    URIRef('https://inspire.ec.europa.eu/theme'),
+                    self.language), keyword)
+                if keyword_iri:
+                    l.append((self.datasetid, DCAT.theme, keyword_iri))
+                    continue
+            l.append((self.datasetid, DCAT.keyword,
+                Literal(keyword, lang=self.language)))
+        return l
+
+    @property
+    def map_access_rights(self):
+        return find_iri(self.isoxml, './gmd:identificationInfo/'
+            'gmd:MD_DataIdentification/gmd:resourceConstraints/'
+            'gmd:MD_LegalConstraints/gmd:otherConstraints/'
+            'gco:CharacterString', self.datasetid, DCT.accessRights,
+            thesaurus=URIRef('http://inspire.ec.europa.eu/'
+                'metadata-codelist/LimitationsOnPublicAccess'))        
+
+    @property
     def map_organizations(self):
         l = []
+        role_map = {
+            'resourceProvider': GEODCAT.resourceProvider,
+            'custodian': GEODCAT.custodian,
+            'owner': DCT.rightsHolder,
+            'user': GEODCAT.user,
+            'distributor': GEODCAT.distributor,
+            'originator': GEODCAT.originator,
+            'principalInvestigator': GEODCAT.principalInvestigator,
+            'processor': GEODCAT.processor,
+            'publisher': DCT.publisher,
+            'author': DCT.creator,
+            'pointOfContact': DCAT.contactPoint
+            }
         for elem in self.isoxml.findall('./gmd:identificationInfo/'
             'gmd:MD_DataIdentification/gmd:pointOfContact', ns):
             r = elem.find('./gmd:CI_ResponsibleParty/'
                 'gmd:role/gmd:CI_RoleCode', namespaces=ns)
-            if not r:
+            if r is None:
                 continue
-            role_iso = r.text or r.get('codeListValue')
-            if not role_iso:
+            role = r.text or r.get('codeListValue')
+            if not role or not role in role_map:
                 continue
             node = BNode()
-            if role_iso == 'pointOfContact':
+            if role == 'pointOfContact':
                 triples = find_literal(elem, './gmd:CI_ResponsibleParty/'
-                    'gmd:organisationName/gco:CharacterString', node, VCARD.fn)
+                    'gmd:individualName/gco:CharacterString', node, VCARD.fn)
+                triples += find_literal(elem, './gmd:CI_ResponsibleParty/'
+                    'gmd:organisationName/gco:CharacterString', node,
+                    VCARD['organization-name'] if triples else VCARD.fn)
                 triples += find_iri(elem, './gmd:CI_ResponsibleParty/'
                     'gmd:contactInfo/gmd:CI_Contact/gmd:address/'
                     'gmd:CI_Address/gmd:electronicMailAddress/'
-                    'gco:CharacterString', node, VCARD.hasEmail)
+                    'gco:CharacterString', node, VCARD.hasEmail,
+                    transform='email')
                 triples += find_iri(elem, './gmd:CI_ResponsibleParty/'
                     'gmd:contactInfo/gmd:CI_Contact/'
                     'gmd:onlineResource/gmd:CI_OnlineResource/'
@@ -160,33 +254,68 @@ class IsoToDcat:
                 triples += find_iri(elem, './gmd:CI_ResponsibleParty/'
                     'gmd:contactInfo/gmd:CI_Contact/gmd:phone/'
                     'gmd:CI_Telephone/gmd:voice/gco:CharacterString',
-                    node, VCARD.hasTelephone)
+                    node, VCARD.hasTelephone, transform='phone')
                 if triples:
                     triples.append((self.datasetid, DCAT.contactPoint, node))
                     triples.append((node, RDF.type, VCARD.Kind))
                     l += triples
             else:
-                role_iri = 'https://inspire.ec.europa.eu/metadata-codelist/' \
-                    'ResponsiblePartyRole/{}'.format(role_iso)
-                #TODO
+                predicate = role_map[role]
+                triples = find_literal(elem, './gmd:CI_ResponsibleParty/'
+                    'gmd:organisationName/gco:CharacterString', node, FOAF.name)
+                triples += find_iri(elem, './gmd:CI_ResponsibleParty/'
+                    'gmd:contactInfo/gmd:CI_Contact/gmd:address/'
+                    'gmd:CI_Address/gmd:electronicMailAddress/'
+                    'gco:CharacterString', node, FOAF.mbox,
+                    transform='email')
+                triples += find_iri(elem, './gmd:CI_ResponsibleParty/'
+                    'gmd:contactInfo/gmd:CI_Contact/'
+                    'gmd:onlineResource/gmd:CI_OnlineResource/'
+                    'gmd:linkage/gmd:URL', node, FOAF.workplaceHomepage)
+                triples += find_iri(elem, './gmd:CI_ResponsibleParty/'
+                    'gmd:contactInfo/gmd:CI_Contact/gmd:phone/'
+                    'gmd:CI_Telephone/gmd:voice/gco:CharacterString',
+                    node, FOAF.phone, transform='phone')
+                if triples:
+                    triples.append((self.datasetid, predicate, node))
+                    triples.append((node, RDF.type, FOAF.Agent))
+                    l += triples
         return l
 
-def find_literal(elem, path, subject, predicate, datatype=None, language=None):
-    value = elem.findtext(path, namespaces=ns)
-    if value:
+def find_literal(elem, path, subject, predicate, multi=False, datatype=None, language=None):
+    l = []
+    for sub in elem.findall(path, namespaces=ns):
+        value = sub.text
+        if not value:
+            continue
         if language:
-            return [(subject, predicate, Literal(value, lang=language))]
-        if datatype:
-            return [(subject, predicate, Literal(value, datatype=datatype))]
-        return [(subject, predicate, Literal(value))]
-    return []
+            l.append((subject, predicate, Literal(value, lang=language)))
+        elif datatype:
+            l.append((subject, predicate, Literal(value, datatype=datatype)))
+        else:
+            l.append((subject, predicate, Literal(value)))
+        if not multi:
+            break
+    return l
 
-def find_iri(elem, path, subject, predicate):
-    value = elem.findtext(path, namespaces=ns)
-    if not value or forbidden_char(value):
-        return []
-    return [(subject, predicate, URIRef(value))]
-    
+def find_iri(elem, path, subject, predicate, multi=False, transform=None, thesaurus=None):
+    l = []
+    for sub in elem.findall(path, namespaces=ns):
+        value = sub.text
+        if not value or forbidden_char(value):
+            continue
+        if transform == 'email':
+            value = owlthing_from_email(value)
+        elif transform == 'phone':
+            value = owlthing_from_tel(value)
+        elif thesaurus:
+            value = Thesaurus.concept_iri((thesaurus, self.language), value)
+            if not value:
+                continue
+        l.append((subject, predicate, URIRef(value)))
+        if not multi:
+            break
+    return l    
 
 def wns(tag):
     """Explicite le tag en remplaçant le préfixe par l'URL de l'espace de nommage correspondant.
