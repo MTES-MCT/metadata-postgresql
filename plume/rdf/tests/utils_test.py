9df974eeff502ad44e480a7d97e16c7d78428ef4
alhyss <55992003+alhyss@users.noreply.github.com>
2022-02-07 16:04:04 +0100

    Update utils_test.py
    Ajout d'un test pour `wkt_with_srid`.

--- plume/rdf/tests/utils_test.py
+++ plume/rdf/tests/utils_test.py
@@ -4,12 +4,45 @@ from uuid import uuid4
 
 from plume.rdf.rdflib import Literal, URIRef
 from plume.rdf.utils import sort_by_language, pick_translation, \
-    path_from_n3, int_from_duration, duration_from_int, str_from_duration
+    path_from_n3, int_from_duration, duration_from_int, str_from_duration, \
+    wkt_with_srid
 from plume.rdf.namespaces import PlumeNamespaceManager, DCT, XSD
 
 nsm = PlumeNamespaceManager()
 
 class UtilsTestCase(unittest.TestCase):
+
+    def test_wkt_with_srid(self):
+        """Explicitation du référentiel dans un WKT.
+        
+        """
+        self.assertEqual(
+            wkt_with_srid('POINT(651796.32814998598769307 6862298.58582336455583572)',
+                'OGC:CRS84'),
+            '<http://www.opengis.net/def/crs/OGC/1.3/CRS84> ' \
+            'POINT(651796.32814998598769307 6862298.58582336455583572)'
+            )
+        self.assertEqual(
+            wkt_with_srid('POINT(651796.32814998598769307 6862298.58582336455583572)',
+                'EPSG:2154'),
+            '<http://www.opengis.net/def/crs/EPSG/0/2154> ' \
+            'POINT(651796.32814998598769307 6862298.58582336455583572)'
+            )
+        self.assertEqual(
+            wkt_with_srid('POINT(651796.32814998598769307 6862298.58582336455583572)',
+                'IGNF:RGM04UTM38S'),
+            '<https://registre.ign.fr/ign/IGNF/IGNF.xml#RGM04UTM38S> ' \
+            'POINT(651796.32814998598769307 6862298.58582336455583572)'
+            )
+        self.assertIsNone(wkt_with_srid(None, 'IGNF:RGM04UTM38S'))
+        self.assertIsNone(
+            wkt_with_srid('POINT(651796.32814998598769307 6862298.58582336455583572)',
+                'CHOSE:CRS84'))
+        self.assertEqual(
+            wkt_with_srid('POINT(651796.32814998598769307 6862298.58582336455583572)',
+                None),
+            'POINT(651796.32814998598769307 6862298.58582336455583572)'
+            )
 
     def test_sort_by_language(self):
         """Tri d'une liste de valeurs litérales selon leur langue.
