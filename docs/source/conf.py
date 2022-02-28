824e0d8dcc606618b8b18f7d64ef1fd2c4d6c739
alhyss <55992003+alhyss@users.noreply.github.com>
2022-02-28 14:59:25 +0100

    Update conf.py
    Avec le thème de ReadTheDocs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,6 +33,7 @@ release = '0.3.0-beta'
 extensions = [
     'sphinx.ext.githubpages',
     'myst_parser', # Markdown support
+    'sphinx_rtd_theme', # ReadTheDocs theme
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon' # NumPy support
 ]
@@ -65,7 +66,7 @@ exclude_patterns = [
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
