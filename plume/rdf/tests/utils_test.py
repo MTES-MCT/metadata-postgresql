fcf09040dfbc04a4ee711e98d87db402c56a9c31
alhyss <55992003+alhyss@users.noreply.github.com>
2022-03-21 14:52:32 +0100

    Update utils_test.py
    Ajout d'un test pour `export_format_from_extension`.

--- plume/rdf/tests/utils_test.py
+++ plume/rdf/tests/utils_test.py
@@ -7,12 +7,27 @@ from plume.rdf.utils import sort_by_language, pick_translation, \
     path_from_n3, int_from_duration, duration_from_int, str_from_duration, \
     wkt_with_srid, split_rdf_wkt, str_from_datetime, str_from_date, \
     str_from_time, datetime_from_str, date_from_str, time_from_str, \
-    str_from_decimal, decimal_from_str, main_datatype, geomtype_from_wkt
+    str_from_decimal, decimal_from_str, main_datatype, geomtype_from_wkt, \
+    export_format_from_extension
 from plume.rdf.namespaces import PlumeNamespaceManager, DCT, XSD, RDF
 
 nsm = PlumeNamespaceManager()
 
 class UtilsTestCase(unittest.TestCase):
+
+    def test_export_format_from_extension(self):
+        """Choix du bon format d'export, selon l'extension et un format pré-sélectionné.
+        
+        """
+        self.assertEqual(export_format_from_extension('.rdf'), 'pretty-xml')
+        self.assertEqual(export_format_from_extension('.rdf', 'pretty-xml'), 'pretty-xml')
+        self.assertEqual(export_format_from_extension('.rdf', 'xml'), 'xml')
+        self.assertIsNone(export_format_from_extension('.truc'))
+        self.assertEqual(export_format_from_extension('.truc', 'xml'), 'xml')
+        self.assertIsNone(export_format_from_extension(''))
+        self.assertEqual(export_format_from_extension('', 'turtle'), 'turtle')
+        self.assertIsNone(export_format_from_extension(None))
+        self.assertEqual(export_format_from_extension(None, 'turtle'), 'turtle')
 
     def test_geomtype_from_wkt(self):
         """Identification du type de géométrie d'une représentation WKT.
