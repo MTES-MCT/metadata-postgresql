2815481e0dc81440687bf1bc881d7e70458243d1
wreatched <didier.leclerc@developpement-durable.gouv.fr>
2022-03-30 09:12:01 +0200

    Version 0.3 Bëta
    gestion des bibliothèques
    
    Version < 3.8.0

--- plume/bibli_install/bibli_install.py
+++ plume/bibli_install/bibli_install.py
@@ -2,6 +2,7 @@
 # créé sept 2021
 import os.path
 import subprocess
+from platform import python_version
 
 #==================================================
 def manageLibrary(mBibli) :
@@ -19,6 +20,10 @@ def manageLibrary(mBibli) :
        mPathPerso = os.path.dirname(__file__) + '\\wheel-0.37.1-py2.py3-none-any.whl'
        mPathPerso = mPathPerso.replace("\\","/")
        subprocess.check_call(['python3', '-m', 'pip', 'install', mPathPerso])
+       # SIX
+       mPathPerso = os.path.dirname(__file__) + '\\six-1.16.0-py2.py3-none-any.whl'
+       mPathPerso = mPathPerso.replace("\\","/")
+       subprocess.check_call(['python3', '-m', 'pip', 'install', mPathPerso])
        # ISODATE
        mPathPerso = os.path.dirname(__file__) + '\\isodate-0.6.1-py2.py3-none-any.whl'
        mPathPerso = mPathPerso.replace("\\","/")
@@ -31,14 +36,15 @@ def manageLibrary(mBibli) :
        mPathPerso = os.path.dirname(__file__) + '\\setuptools-61.2.0-py3-none-any.whl'
        mPathPerso = mPathPerso.replace("\\","/")
        subprocess.check_call(['python3', '-m', 'pip', 'install', mPathPerso])
-       # ZIPP
-       mPathPerso = os.path.dirname(__file__) + '\\zipp-3.7.0-py3-none-any.whl'
-       mPathPerso = mPathPerso.replace("\\","/")
-       subprocess.check_call(['python3', '-m', 'pip', 'install', mPathPerso])
-       # IMPORTLIB-METADATA
-       mPathPerso = os.path.dirname(__file__) + '\\importlib_metadata-4.11.3-py3-none-any.whl'
-       mPathPerso = mPathPerso.replace("\\","/")
-       subprocess.check_call(['python3', '-m', 'pip', 'install', mPathPerso])
+       if python_version() < '3.8.0':
+          # ZIPP
+          mPathPerso = os.path.dirname(__file__) + '\\zipp-3.7.0-py3-none-any.whl'
+          mPathPerso = mPathPerso.replace("\\","/")
+          subprocess.check_call(['python3', '-m', 'pip', 'install', mPathPerso])
+          # IMPORTLIB-METADATA
+          mPathPerso = os.path.dirname(__file__) + '\\importlib_metadata-4.11.3-py3-none-any.whl'
+          mPathPerso = mPathPerso.replace("\\","/")
+          subprocess.check_call(['python3', '-m', 'pip', 'install', mPathPerso])
        # RDFLIB
        mPathPerso = os.path.dirname(__file__) + '\\rdflib-6.1.1-py3-none-any.whl'
        mPathPerso = mPathPerso.replace("\\","/")
