f04d8ba6fe6a950b3a6d1cf63d7b7281469b61ef
alhyss <55992003+alhyss@users.noreply.github.com>
2022-04-06 13:56:58 +0200

    Create config.py

---
+++ plume/pg/config.py
@@ -0,0 +1,14 @@
+"""Configuration.
+
+"""
+
+PLUME_PG_MIN_VERSION = '0.1.0'
+"""Plus petite version de l'extension PlumePg compatible avec la version courante de Plume.
+
+"""
+
+PLUME_PG_MAX_VERSION = '1.0.0'
+"""Plus petite version de l'extension PlumePg présumée non rétro-compatible avec la version courante de Plume.
+
+"""
+
