13df09c825ab72ddf195569bcc2a098a12bd5380
alhyss <55992003+alhyss@users.noreply.github.com>
2022-04-12 14:23:23 +0200

    Update rdflib.py
    Suppression de l'appel de `bibli_install.manageLibrary`, maintenant géré en amont.

--- plume/rdf/rdflib.py
+++ plume/rdf/rdflib.py
@@ -11,25 +11,13 @@ Et jamais :
 
     >>> from rdflib.term import URIRef
 
-Plume requiert RDFLib 6.0.
-
-Le présent module s'assure d'appeler l'installeur interne de
-Plume si la bibliothèque n'est pas disponible.
-
 References
 ----------
 https://rdflib.readthedocs.io/en/stable/index.html
 
 """
 
-try:
-    from rdflib.graph import Graph
-except:
-    from plume.bibli_install.bibli_install import manageLibrary
-    # installe RDFLib si n'est pas déjà disponible
-    manageLibrary("RDFLIB")
-    from rdflib.graph import Graph
-
+from rdflib.graph import Graph
 from rdflib.term import BNode, Literal, URIRef
 from rdflib.util import from_n3
 from rdflib.namespace import Namespace, NamespaceManager
