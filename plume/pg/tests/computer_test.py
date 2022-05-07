6b8d9fd248f47819814ae9f04f3a5f28b77559d9
alhyss <55992003+alhyss@users.noreply.github.com>
2022-05-07 18:29:01 +0200

    Update computer_test.py
    Ajout d'un test pour la fonction `has_computation_method`

--- plume/pg/tests/computer_test.py
+++ plume/pg/tests/computer_test.py
@@ -1,7 +1,10 @@
 
 import unittest, psycopg2
 
-from plume.pg.computer import datetime_parser
+from plume.pg.computer import datetime_parser, has_computation_method
+from plume.rdf.properties import PlumeProperty
+from plume.rdf.widgetsdict import WidgetsDict
+from plume.rdf.namespaces import DCAT, DCT, PlumeNamespaceManager
 from plume.pg.tests.connection import ConnectionString
 
 class ComputerTestCase(unittest.TestCase):
@@ -25,6 +28,30 @@ class ComputerTestCase(unittest.TestCase):
         conn.close()
         cr = datetime_parser(*result[0])
         self.assertEqual(cr.str_value, '20/04/2022 16:46:35')
+
+    def test_has_computing_method(self):
+        """Détermine si une catégorie, clé ou propriété a une méthode de calcul associée.
+
+        """
+        # --- simple chemin ---
+        self.assertTrue(has_computation_method(DCT.title))
+        self.assertFalse(has_computation_method(DCAT.landingPage))
+
+        # --- propriété ---
+        prop = PlumeProperty(origin='unknown', nsm=PlumeNamespaceManager(),
+            predicate=DCT.modified)
+        self.assertTrue(has_computation_method(prop))
+        prop = PlumeProperty(origin='unknown', nsm=PlumeNamespaceManager(),
+            predicate=DCT.issued)
+        self.assertFalse(has_computation_method(prop))
+
+        # --- clé de dictionnaire de widgets ---
+        widgetsdict = WidgetsDict()
+        widgetkey = widgetsdict.root.search_from_path(DCT.conformsTo)
+        self.assertTrue(has_computation_method(widgetkey))
+        widgetkey = widgetsdict.root.search_from_path(DCAT.theme)
+        self.assertFalse(has_computation_method(widgetkey))
+
 
 if __name__ == '__main__':
     unittest.main()
