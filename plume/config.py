ef8e37836b6c8e01db17fa2c9cce4241c7e5ce4e
alhyss <55992003+alhyss@users.noreply.github.com>
2022-05-20 18:50:23 +0200

    Update config.py
    Ajout des paramètres autrefois gérés dans le fichier `plume/pg/config.py` + numéro de version.

--- plume/config.py
+++ plume/config.py
@@ -1,5 +1,21 @@
 """Configuration.
+
 """
+
+# à actualiser à chaque nouvelle version, d'autant que de besoin :
+
+PLUME_VERSION = 'v0.4 bêta'
+"""Version courante de Plume, sous forme litérale."""
+
+PLUME_VERSION_TPL = (0, 4, 0)
+"""Version courante de Plume, sous forme de tuple (version majeure, version mineure, correctif)."""
+
+PLUME_PG_MIN_VERSION = '0.1.0'
+"""Plus petite version de l'extension PlumePg compatible avec la version courante de Plume."""
+
+PLUME_PG_MAX_VERSION = '1.0.0'
+"""Plus petite version de l'extension PlumePg présumée non rétro-compatible avec la version courante de Plume."""
+
 # ===================
 # = for Plugin PLUME
 # - DL
@@ -11,7 +27,4 @@ VALUEDEFAUTFILEHELPHTML = "https://snum.scenari-community.org/Plume/Documentatio
 URLCSWDEFAUT            = "http://ogc.geo-ide.developpement-durable.gouv.fr/csw/dataset-harvestable,https://wxs.ign.fr/catalogue/csw"
 URLCSWIDDEFAUT          = "fr-120066022-jdd-23d6b4cd-5a3b-4e10-83ae-d8fdad9b04ab,IGNF_ADMIN_EXPRESS_3-1.xml"
 
-# ===================
-# = for biblioth�ques PLUME
-# - LL
-# ===================
+
