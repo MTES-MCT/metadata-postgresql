222a83a8dd4b0bcc6bf2883f32112e7de91400ca
alhyss <55992003+alhyss@users.noreply.github.com>
2022-06-14 18:15:03 +0200

    Create plume_pg--0.1.0--0.1.1.sql
    Script de mise à jour qui ne contient aucune commande, puisque la v0.1.1 concerne les serveurs pour lesquels la mise à jour depuis la v0.1.0 échouait.

---
+++ postgresql/plume_pg--0.1.0--0.1.1.sql
@@ -0,0 +1,52 @@
+\echo Use "ALTER EXTENSION plume_pg UPDATE TO '0.1.1'" to load this file. \quit
+-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+--
+-- PlumePg - Système de gestion des métadonnées locales, version 0.1.1
+-- > Script de mise à jour depuis la version 0.1.0
+--
+-- Copyright République Française, 2022.
+-- Secrétariat général du Ministère de la transition écologique, du
+-- Ministère de la cohésion des territoires et des relations avec les
+-- collectivités territoriales et du Ministère de la Mer.
+-- Service du numérique.
+--
+-- contributeurs : Leslie Lemaire (SNUM/UNI/DRC).
+-- 
+-- mél : drc.uni.snum.sg@developpement-durable.gouv.fr
+--
+-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+--
+-- Documentation :
+-- https://mtes-mct.github.io/metadata-postgresql/usage/gestion_plume_pg.html
+-- https://snum.scenari-community.org/Plume/Documentation/ (en cours de rédaction)
+--
+-- GitHub :
+-- https://github.com/MTES-MCT/metadata-postgresql
+-- 
+-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+--
+-- Ce logiciel est un programme informatique complémentaire au système de
+-- gestion de base de données PosgreSQL ("https://www.postgresql.org/"). Il
+-- met en place côté serveur les objets nécessaires à la gestion des 
+-- métadonnées, en complément du plugin QGIS Plume.
+--
+-- Ce logiciel est régi par la licence GNU Affero General Public License v3.0
+-- or later. Lien SPDX : https://spdx.org/licenses/AGPL-3.0-or-later.html.
+--
+-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+--
+-- dépendances : pgcrypto
+--
+-- schéma contenant les objets : z_plume
+--
+-- objets créés par le script : néant.
+-- objets modifiés par le script : néant.
+--
+-- Le présent script a pour seul effet de modifier le numéro de version
+-- de PlumePg. La version 0.1.1 est une version corrective dont le
+-- seul objet est de permettre la mise à jour depuis la version 0.0.1
+-- sous PostgreSQL 10 et 11. Le présent script concerne les serveurs
+-- qui n'étaient pas bloqués en 0.0.1.
+--
+-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
