922bc37a3c72398b2aa0b8c4ac733f44c67342cd
alhyss <55992003+alhyss@users.noreply.github.com>
2023-05-04 20:41:03 +0200

    Update map_test.py
    Ajout de tests pour le mapping des référentiels de coordonnées.

--- plume/iso/tests/map_test.py
+++ plume/iso/tests/map_test.py
@@ -6,15 +6,13 @@ from plume.rdf.utils import (
     owlthing_from_tel
 )
 from plume.iso.map import (
-    find_iri, find_literal, parse_xml, ISO_NS
+    find_iri, find_literal, parse_xml, ISO_NS, normalize_crs,
+    IsoToDcat
 )
 from plume.rdf.namespaces import DCT, FOAF, DCAT, PLUME
 from plume.rdf.rdflib import Literal, URIRef
 
 class IsoMapTestCase(unittest.TestCase):
-
-    def test_find_literal_list_of_paths(self):
-        """Application de la fonction find_literal à une liste de chemins."""
     
     @classmethod
     def setUpClass(cls):
@@ -34,9 +32,9 @@ class IsoMapTestCase(unittest.TestCase):
             data_from_file(abspath('rdf/tests/samples/iso_geolittoral_sentier_du_littoral.xml'))
         )
         cls.ALL = {
-            'iso_ignf_bdaltir_2_0': cls.IGN_BDALTI,
             'iso_datara_territoire_fonds_air_bois': cls.CSW_DATARA_FOND_AB,
             'iso_geoide_zac_paris': cls.CSW_GEOIDE_ZAC_75,
+            'iso_ignf_bdaltir_2_0': cls.IGN_BDALTI,
             'iso_geolittoral_sentier_du_littoral': cls.CSW_GEOLITTORAL_SENTIER
         }
     
@@ -332,6 +330,128 @@ class IsoMapTestCase(unittest.TestCase):
                 )
             ]
         )
+
+    def test_normalize_crs(self):
+        """Vérifie que la fonction qui extrait un référentiel de coordonnées d'une chaîne de caractères réagit bien dans les cas de figure anticipés."""
+
+        self.assertEqual(
+            normalize_crs('2154'),
+            'http://www.opengis.net/def/crs/EPSG/0/2154'
+        )
+
+        self.assertEqual(
+            normalize_crs('http://www.opengis.net/def/crs/EPSG/0/2154'),
+            'http://www.opengis.net/def/crs/EPSG/0/2154'
+        )
+
+        self.assertEqual(
+            normalize_crs('EPSG2154'),
+            'http://www.opengis.net/def/crs/EPSG/0/2154'
+        )
+
+        self.assertEqual(
+            normalize_crs('EPSG:2154'),
+            'http://www.opengis.net/def/crs/EPSG/0/2154'
+        )
+
+        self.assertEqual(
+            normalize_crs('EPSG 2154'),
+            'http://www.opengis.net/def/crs/EPSG/0/2154'
+        )
+
+        self.assertEqual(
+            normalize_crs('RGF93 / Lambert-93 (EPSG:2154)'),
+            'http://www.opengis.net/def/crs/EPSG/0/2154'
+        )
+
+        self.assertEqual(
+            normalize_crs('xxxxxEPSG:2154.'),
+            'http://www.opengis.net/def/crs/EPSG/0/2154'
+        )
+
+        self.assertEqual(
+            normalize_crs('urn:ogc:def:crs:EPSG::310566307'),
+            'http://www.opengis.net/def/crs/EPSG/0/310566307'
+        )
+
+        self.assertEqual(
+            normalize_crs('https://registre.ign.fr/ign/IGNF/crs/IGNF/AMANU63UTM7S'),
+            'https://registre.ign.fr/ign/IGNF/crs/IGNF/AMANU63UTM7S'
+        )
+
+        self.assertEqual(
+            normalize_crs('IGNF:AMANU63UTM7S'),
+            'https://registre.ign.fr/ign/IGNF/crs/IGNF/AMANU63UTM7S'
+        )
+
+        self.assertEqual(
+            normalize_crs('IGNF:RGAF09UTM20.MART87'),
+            'https://registre.ign.fr/ign/IGNF/crs/IGNF/RGAF09UTM20.MART87'
+        )
+
+class IsoToDcatTestCase(unittest.TestCase):
+    
+    @classmethod
+    def setUpClass(cls):
+        """Import des fichiers de test.
+        
+        """
+        cls.IGN_BDALTI = data_from_file(abspath('rdf/tests/samples/iso_ignf_bdaltir_2_0.xml'))
+        cls.CSW_DATARA_FOND_AB = data_from_file(
+            abspath('rdf/tests/samples/iso_datara_territoire_fonds_air_bois.xml')
+        )
+        cls.CSW_GEOIDE_ZAC_75 = data_from_file(abspath('rdf/tests/samples/iso_geoide_zac_paris.xml'))
+        cls.CSW_GEOLITTORAL_SENTIER = data_from_file(
+            abspath('rdf/tests/samples/iso_geolittoral_sentier_du_littoral.xml')
+        )
+        cls.ALL = {
+            'iso_datara_territoire_fonds_air_bois': cls.CSW_DATARA_FOND_AB,
+            'iso_geoide_zac_paris': cls.CSW_GEOIDE_ZAC_75,
+            'iso_ignf_bdaltir_2_0': cls.IGN_BDALTI,
+            'iso_geolittoral_sentier_du_littoral': cls.CSW_GEOLITTORAL_SENTIER
+        }
+
+    def test_map_epsg(self):
+        """Récupération des référentiels de coordonnées dans les fichiers de test."""
+        dataset_id = DatasetId()
+
+        samples = {
+            self.CSW_GEOIDE_ZAC_75: [
+                (dataset_id, DCT.conformsTo, URIRef('http://www.opengis.net/def/crs/EPSG/0/2154'))
+            ],
+            self.IGN_BDALTI: [
+                # les référentiels non référencés dans les vocabulaires sont mis en commentaire
+                (dataset_id, DCT.conformsTo, URIRef('https://registre.ign.fr/ign/IGNF/crs/IGNF/RGF93LAMB93.IGN69')),
+                (dataset_id, DCT.conformsTo, URIRef('https://registre.ign.fr/ign/IGNF/crs/IGNF/RGF93LAMB93.IGN78C')),
+                (dataset_id, DCT.conformsTo, URIRef('https://registre.ign.fr/ign/IGNF/crs/IGNF/RGFG95UTM22.GUYA77')),
+                (dataset_id, DCT.conformsTo, URIRef('https://registre.ign.fr/ign/IGNF/crs/IGNF/RGM04UTM38S.MAYO53')),
+                (dataset_id, DCT.conformsTo, URIRef('https://registre.ign.fr/ign/IGNF/crs/IGNF/RGR92G.REUN89')),
+                (dataset_id, DCT.conformsTo, URIRef('https://registre.ign.fr/ign/IGNF/crs/IGNF/RGSPM06U21.STPM50')),
+                # (dataset_id, DCT.conformsTo, URIRef('https://registre.ign.fr/ign/IGNF/crs/IGNF/WGS84UTM20.GUAD88')),
+                # (dataset_id, DCT.conformsTo, URIRef('https://registre.ign.fr/ign/IGNF/crs/IGNF/WGS84UTM20.GUAD88MG')),
+                # (dataset_id, DCT.conformsTo, URIRef('https://registre.ign.fr/ign/IGNF/crs/IGNF/WGS84UTM20.GUAD88SB')),
+                # (dataset_id, DCT.conformsTo, URIRef('https://registre.ign.fr/ign/IGNF/crs/IGNF/WGS84UTM20.GUAD88SM')),
+                # (dataset_id, DCT.conformsTo, URIRef('https://registre.ign.fr/ign/IGNF/crs/IGNF/WGS84UTM20.GUAD92LD')),
+                # (dataset_id, DCT.conformsTo, URIRef('https://registre.ign.fr/ign/IGNF/crs/IGNF/WGS84UTM20.MART87'))
+            ],
+            self.CSW_DATARA_FOND_AB: [
+                (dataset_id, DCT.conformsTo, URIRef('http://www.opengis.net/def/crs/EPSG/0/2154'))
+            ],
+            self.CSW_GEOLITTORAL_SENTIER: [
+                (dataset_id, DCT.conformsTo, URIRef('http://www.opengis.net/def/crs/EPSG/0/2154')),
+                (dataset_id, DCT.conformsTo, URIRef('http://www.opengis.net/def/crs/EPSG/0/5490')),
+                (dataset_id, DCT.conformsTo, URIRef('http://www.opengis.net/def/crs/EPSG/0/4467')),
+                (dataset_id, DCT.conformsTo, URIRef('http://www.opengis.net/def/crs/EPSG/0/2972'))
+            ]
+        }
+        for sample_name in self.ALL:
+            with self.subTest(sample=sample_name):
+                sample = self.ALL[sample_name]
+                itd = IsoToDcat(sample, datasetid=dataset_id)
+                self.assertListEqual(
+                    sorted(itd.map_epsg),
+                    sorted(samples[sample])
+                )
 
 if __name__ == '__main__':
     unittest.main()
