204d70539543a9c326a06ec56f5be6b59bf2ed0f
alhyss <55992003+alhyss@users.noreply.github.com>
2023-05-05 14:53:04 +0200

    unilang as boolean
    Déclaration du champ `unilang` des tables `z_plume.meta_categorie` et `z_plume.meta_template_categories` comme booléen. Ses valeurs était déjà traitées comme telles partout où elles étaient utilisées.
    
    Cf. issue #110

--- postgresql/plume_pg--0.2.0--0.3.0.sql
+++ postgresql/plume_pg--0.2.0--0.3.0.sql
@@ -99,3 +99,81 @@ UPDATE z_plume.meta_shared_categorie
     SET label = 'Statut',
         description = 'Maturité de la distribution.'
     WHERE path = 'dcat:distribution / adms:status' ;
+
+DROP VIEW z_plume.meta_template_categories_full ;
+
+-- Table: z_plume.meta_categorie
+
+ALTER TABLE z_plume.meta_categorie
+    ALTER COLUMN unilang TYPE boolean USING (unilang::boolean) ;
+
+-- Table: z_plume.meta_template_categories
+
+ALTER TABLE z_plume.meta_template_categories
+    ALTER COLUMN unilang TYPE boolean USING (unilang::boolean) ;
+
+-- View: z_plume.meta_template_categories_full
+
+CREATE VIEW z_plume.meta_template_categories_full AS (
+    SELECT
+        tc.tplcat_id,
+        t.tpl_label,
+        coalesce(tc.shrcat_path, tc.loccat_path) AS path,
+        c.origin,
+        coalesce(tc.label, c.label) AS label,
+        coalesce(tc.description, c.description) AS description,
+        coalesce(tc.special, c.special) AS special,
+        c.is_node,
+        coalesce(tc.datatype, c.datatype) AS datatype,
+        coalesce(tc.is_long_text, c.is_long_text) AS is_long_text,
+        coalesce(tc.rowspan, c.rowspan) AS rowspan,
+        coalesce(tc.placeholder, c.placeholder) AS placeholder,
+        coalesce(tc.input_mask, c.input_mask) AS input_mask,
+        coalesce(tc.is_multiple, c.is_multiple) AS is_multiple,
+        coalesce(tc.unilang, c.unilang) AS unilang,
+        coalesce(tc.is_mandatory, c.is_mandatory) AS is_mandatory,
+        coalesce(tc.sources, c.sources) AS sources,
+        coalesce(tc.geo_tools, c.geo_tools) AS geo_tools,
+        coalesce(tc.compute, c.compute) AS compute,
+        coalesce(tc.template_order, c.template_order) AS template_order,
+        tc.is_read_only,
+        tb.tab_label AS tab,
+        coalesce(tc.compute_params, c.compute_params) AS compute_params
+        FROM z_plume.meta_template_categories AS tc
+            LEFT JOIN z_plume.meta_categorie AS c
+                ON coalesce(tc.shrcat_path, tc.loccat_path) = c.path
+            LEFT JOIN z_plume.meta_template AS t
+                ON tc.tpl_id = t.tpl_id
+            LEFT JOIN z_plume.meta_tab AS tb 
+                ON tc.tab_id = tb.tab_id
+        WHERE t.enabled
+    ) ;
+
+GRANT SELECT ON TABLE z_plume.meta_template_categories_full TO public ;
+
+COMMENT ON VIEW z_plume.meta_template_categories_full IS 'Description complète des modèles de formulaire (rassemble les informations de meta_categorie et meta_template_categories).' ;
+
+COMMENT ON COLUMN z_plume.meta_template_categories_full.tplcat_id IS 'Identifiant unique.' ;
+COMMENT ON COLUMN z_plume.meta_template_categories_full.tpl_label IS 'Nom du modèle.' ;
+COMMENT ON COLUMN z_plume.meta_template_categories_full.path IS 'Chemin N3 / identifiant de la catégorie.' ;
+COMMENT ON COLUMN z_plume.meta_template_categories_full.origin IS 'Origine de la catégorie : ''shared'' pour une catégorie commune, ''local'' pour une catégorie locale supplémentaire.' ;
+COMMENT ON COLUMN z_plume.meta_template_categories_full.label IS 'Libellé de la catégorie. Le cas échéant, cette valeur se substituera pour le modèle considéré à la valeur renseignée dans le schéma des métadonnées communes.' ;
+COMMENT ON COLUMN z_plume.meta_template_categories_full.description IS 'Description de la catégorie. Sera affiché sous la forme d''un texte d''aide dans le formulaire. Le cas échéant, cette valeur se substituera pour le modèle considéré à la valeur renseignée dans le schéma des métadonnées communes.' ;
+COMMENT ON COLUMN z_plume.meta_template_categories_full.special IS 'Le cas échéant, mise en forme spécifique à appliquer au champ. Valeurs autorisées : ''url'', ''email'', ''phone''. Pour les catégories communes, les modifications apportées à ce champ ne seront pas prises en compte.' ;
+COMMENT ON COLUMN z_plume.meta_template_categories_full.is_node IS 'True si la catégorie est le nom d''un groupe qui contiendra lui-même d''autres catégories et non une catégorie à laquelle sera directement associée une valeur. Par exemple, is_node vaut True pour la catégorie correspondant au point de contact (dcat:contactPoint) et False pour le nom du point de contact (dcat:contactPoint / vcard:fn).' ;
+COMMENT ON COLUMN z_plume.meta_template_categories_full.datatype IS 'Type de valeur attendu pour la catégorie. Cette information détermine notamment la nature des widgets utilisés par Plume pour afficher et éditer les valeurs, ainsi que les validateurs appliqués. Pour les catégories communes, les modifications apportées à ce champ ne seront pas prises en compte.' ;
+COMMENT ON COLUMN z_plume.meta_template_categories_full.is_long_text IS 'True pour une catégorie appelant un texte de plusieurs lignes. Cette information ne sera prise en compte que si le type de valeur (datatype) est ''xsd:string'' ou ''rdf:langString''. Le cas échéant, cette valeur se substituera pour le modèle considéré à la valeur renseignée dans le schéma des métadonnées communes.' ;
+COMMENT ON COLUMN z_plume.meta_template_categories_full.rowspan IS 'Nombre de lignes occupées par le widget de saisie, s''il y a lieu de modifier le comportement par défaut de Plume. La valeur ne sera considérée que si is_long_text vaut True. Le cas échéant, cette valeur se substituera pour le modèle considéré à la valeur renseignée dans le schéma des métadonnées communes.' ;
+COMMENT ON COLUMN z_plume.meta_template_categories_full.placeholder IS 'Valeur fictive pré-affichée en tant qu''exemple dans le widget de saisie, s''il y a lieu. Le cas échéant, cette valeur se substituera pour le modèle considéré à la valeur renseignée dans le schéma des métadonnées communes.' ;
+COMMENT ON COLUMN z_plume.meta_template_categories_full.input_mask IS 'Masque de saisie, s''il y a lieu. La valeur sera ignorée si le widget utilisé pour la catégorie ne prend pas en charge ce mécanisme. Le cas échéant, cette valeur se substituera pour le modèle considéré à la valeur renseignée dans le schéma des métadonnées communes.' ;
+COMMENT ON COLUMN z_plume.meta_template_categories_full.is_multiple IS 'True si la catégorie admet plusieurs valeurs. Pour les catégories commnes, les modifications apportées à ce champ ne seront pas prises en compte.' ;
+COMMENT ON COLUMN z_plume.meta_template_categories_full.unilang IS 'True si la catégorie n''admet plusieurs valeurs que si elles sont dans des langues différentes (par exemple un jeu de données n''a en principe qu''un seul titre, mais il peut être traduit). is_multiple est ignoré quand unilang vaut True. Pour les catégories commnes, les modifications apportées à ce champ ne seront pas prises en compte.' ;
+COMMENT ON COLUMN z_plume.meta_template_categories_full.is_mandatory IS 'True si une valeur doit obligatoirement être saisie pour cette catégorie. Le cas échéant, cette valeur se substituera pour le modèle considéré à la valeur renseignée dans le schéma des métadonnées communes. À noter que ce champ permet de rendre obligatoire une catégorie commune optionnelle, pas l''inverse.' ;
+COMMENT ON COLUMN z_plume.meta_template_categories_full.sources IS 'Pour une catégorie prenant ses valeurs dans un ou plusieurs thésaurus, liste des sources admises. Cette information n''est considérée que pour les catégories communes. Il n''est pas possible d''ajouter des sources ni de les retirer toutes - Plume reviendrait alors à la liste initiale -, mais ce champ permet de restreindre la liste à un ou plusieurs thésaurus jugés les mieux adaptés.' ;
+COMMENT ON COLUMN z_plume.meta_template_categories_full.geo_tools IS 'Pour une catégorie prenant pour valeurs des géométries, liste des fonctionnalités d''aide à la saisie à proposer. Cette information ne sera considérée que si le type (datatype) est ''gsp:wktLiteral''. Le cas échéant, cette valeur se substituera pour le modèle considéré à la valeur renseignée dans le schéma des métadonnées communes.' ;
+COMMENT ON COLUMN z_plume.meta_template_categories_full.compute IS 'Liste des fonctionnalités de calcul à proposer. Cette information ne sera considérée que si une méthode de calcul est effectivement disponible pour la catégorie. Le cas échéant, cette valeur se substituera pour le modèle considéré à la valeur renseignée dans le schéma des métadonnées communes.' ;
+COMMENT ON COLUMN z_plume.meta_template_categories_full.template_order IS 'Ordre d''apparence de la catégorie dans le formulaire. Les plus petits numéros sont affichés en premier. Plume classe les catégories selon l''ordre spécifié par le présent modèle, puis selon l''ordre défini par le schéma des métadonnées communes.' ;
+COMMENT ON COLUMN z_plume.meta_template_categories_full.is_read_only IS 'True si la catégorie est en lecture seule.' ;
+COMMENT ON COLUMN z_plume.meta_template_categories_full.tab IS 'Nom de l''onglet du formulaire où placer la catégorie. Cette information n''est considérée que pour les catégories locales et les catégories communes de premier niveau (par exemple "dcat:distribution / dct:issued" ira nécessairement dans le même onglet que "dcat:distribution"). Pour celles-ci, si aucun onglet n''est fourni, la catégorie ira toujours dans le premier onglet cité pour le modèle ou, à défaut, dans un onglet "Général".' ;
+COMMENT ON COLUMN z_plume.meta_template_categories_full.compute_params IS 'Paramètres des fonctionnalités de calcul, le cas échéant, sous une forme clé-valeur. La clé est le nom du paramètre, la valeur sa valeur. Cette information ne sera considérée que si une méthode de calcul est effectivement disponible pour la catégorie et qu''elle attend un ou plusieurs paramètres. Le cas échéant, cette valeur se substituera pour le modèle considéré à la valeur renseignée dans le schéma des métadonnées communes.' ;
+
