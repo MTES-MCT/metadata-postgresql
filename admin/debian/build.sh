0977cea9ca21b64049fde02c94954ba03449a0e2
alhyss <55992003+alhyss@users.noreply.github.com>
2023-07-03 18:19:27 +0200

    Update build.sh
    Le paquet porte désormais le nom `plume-pg` et plus le nom de l'extension `plume_pg`. Utilisation des variables là où l'un de ces deux noms apparaissait encore.

--- admin/debian/build.sh
+++ admin/debian/build.sh
@@ -3,7 +3,7 @@ set -e
 #--------------------------------------------------------------------------
 # Construction du paquet Debian
 #
-# Syntaxe : deb_build.sh [nom_paquet] [version_plume_pg] [revision_paquet]
+# Syntaxe : deb_build.sh [nom_paquet] [nom_extension] [version_paquet] [revision_paquet]
 #-------------------------------------------------------------------------
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
@@ -11,27 +11,29 @@ PROJECT_DIR=$( cd "$SCRIPT_DIR" && cd ../.. && pwd )
 
 # Usage
 usage() {
-  echo "Usage : $0 [nom_paquet] [version_plume_pg] [revision_paquet]"
+  echo "Usage : $0 [nom_paquet] [nom_extension] [version_paquet] [revision_paquet]"
   exit 1
 }
 [ $# -lt 3 ] && usage
 
 # Lecture arguments
 PKG_NAME=$1
-PKG_VERSION=$2
-DEB_REV=$3
+EXT_NAME=$2
+PKG_VERSION=$3
+DEB_REV=$4
 DEB_ARCH=all
 DEB_FULLNAME=${PKG_NAME}_${PKG_VERSION}-${DEB_REV}_${DEB_ARCH}
 
 # Dossier de base et DEBIAN
 mkdir $DEB_FULLNAME
 cp -a $PROJECT_DIR/admin/debian/DEBIAN $DEB_FULLNAME
-# Mise à jour de la version et du nom du paquet dans les fichiers control, postinst et postrm
+# Mise à jour des variables dans les fichiers control, postinst et postrm
 sed -i s/#PKG_VERSION#/$PKG_VERSION/ $DEB_FULLNAME/DEBIAN/control
-sed -i s/#PKG_VERSION#/$PKG_VERSION/ $DEB_FULLNAME/DEBIAN/postinst
 sed -i s/#PKG_NAME#/$PKG_NAME/ $DEB_FULLNAME/DEBIAN/control
+sed -i s/#PKG_VERSION#/$PKG_VERSION/ $DEB_FULLNAME/DEBIAN/postinst
 sed -i s/#PKG_NAME#/$PKG_NAME/ $DEB_FULLNAME/DEBIAN/postinst
-sed -i s/#PKG_NAME#/$PKG_NAME/ $DEB_FULLNAME/DEBIAN/postrm
+sed -i s/#EXT_NAME#/$EXT_NAME/ $DEB_FULLNAME/DEBIAN/postinst
+sed -i s/#EXT_NAME#/$EXT_NAME/ $DEB_FULLNAME/DEBIAN/postrm
   
 # La documentation
 DEB_DOC_DIR=$DEB_FULLNAME/usr/share/doc/$PKG_NAME
@@ -45,7 +47,7 @@ gzip -n --best $DEB_DOC_DIR/README.md
 # y compris les scripts de mise à jour des versions antérieures
 DEB_LIB=$DEB_FULLNAME/usr/share/$PKG_NAME/$PKG_VERSION
 mkdir -p $DEB_LIB
-cp -f $PROJECT_DIR/postgresql/archives/plume_pg--*--*.sql $PROJECT_DIR/postgresql/plume_pg--*.sql $PROJECT_DIR/postgresql/plume_pg.control $DEB_LIB
+cp -f $PROJECT_DIR/postgresql/archives/$EXT_NAME--*--*.sql $PROJECT_DIR/postgresql/$EXT_NAME--*.sql $PROJECT_DIR/postgresql/$EXT_NAME.control $DEB_LIB
 
 # Ajustement des permissions
 find $DEB_FULLNAME -type f -exec chmod 644 {} \;
