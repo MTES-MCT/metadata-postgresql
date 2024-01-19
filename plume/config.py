94a46f2f4bcdd64ef0a6ec5c7b22186e3b86112b
wreatched <didier.leclerc@developpement-durable.gouv.fr>
2024-01-19 14:57:23 +0100

    Préparation release 1.2.0

--- plume/config.py
+++ plume/config.py
@@ -4,10 +4,10 @@
 
 # à actualiser à chaque nouvelle version, d'autant que de besoin :
 
-PLUME_VERSION = 'v1.1.1'
+PLUME_VERSION = 'v1.2.0'
 """Version courante de Plume, sous forme litérale."""
 
-PLUME_VERSION_TPL = (1, 1, 1)
+PLUME_VERSION_TPL = (1, 2, 0)
 """Version courante de Plume, sous forme de tuple (version majeure, version mineure, correctif)."""
 
 PLUME_PG_MIN_VERSION = '0.3.0'
